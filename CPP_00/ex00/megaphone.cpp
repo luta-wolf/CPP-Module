cd6a7aa24961d326bb38e9743d66f5ec8e999d80
Ethelyn Interdictor <einterdi@pr-c4.kzn.21-school.ru>
2022-07-12 00:59:56 +0300

    cpp00 ex00

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -1,4 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: einterdi <einterdi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/03 14:24:55 by einterdi          #+#    #+#             */
+/*   Updated: 2022/07/12 00:53:56 by einterdi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <iostream>
+#include <cstring>
 
 int main(int argc, char **argv)
+{
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			for (int j = 0; j < strlen(argv[i]); j++)
+				std::cout << (char)(toupper(argv[i][j]));
+			std::cout << " ";
+		}
+		std::cout << std::endl;
+	}
+}
 
