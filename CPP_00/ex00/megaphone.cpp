88dd1be8d93b13f450d11b186105972c7438c650
Ethelyn Interdictor <einterdi@pr-c4.kzn.21-school.ru>
2022-07-12 01:07:46 +0300

    cpp00 ex00

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,12 +6,12 @@
 /*   By: einterdi <einterdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/03 14:24:55 by einterdi          #+#    #+#             */
-/*   Updated: 2022/07/12 00:53:56 by einterdi         ###   ########.fr       */
+/*   Updated: 2022/07/12 01:06:44 by einterdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <cstring>
+#include <iostream>	// для оператора cout
+#include <cstring>	// для функции strlen
 
 int main(int argc, char **argv)
 {
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 		for (int i = 1; i < argc; i++)
 		{
 			for (int j = 0; j < strlen(argv[i]); j++)
-				std::cout << (char)(toupper(argv[i][j]));
+				std::cout << (char)toupper(argv[i][j]);
 			std::cout << " ";
 		}
 		std::cout << std::endl;
