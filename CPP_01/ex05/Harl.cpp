3021386be75dd3b0889e7586ea085dcb9a73dcd6
Ethelyn Interdictor <einterdi@pr-b4.kzn.21-school.ru>
2022-09-02 15:51:42 +0300

    ex05

--- CPP_01/ex05/Harl.cpp
+++ CPP_01/ex05/Harl.cpp
@@ -6,8 +6,51 @@
 /*   By: einterdi <einterdi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/02 15:18:19 by einterdi          #+#    #+#             */
-/*   Updated: 2022/09/02 15:19:42 by einterdi         ###   ########.fr       */
+/*   Updated: 2022/09/02 15:50:33 by einterdi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+Harl::Harl()
+{
+
+}
+
+Harl::~Harl()
+{
+
+}
+
+void Harl::debug( void )
+{
+	std::cout << "I love having extra bacon for my" <<
+		" 7XL-double-cheese-triple-pickle-special- " <<
+		"ketchup burger.I really do!" << std::endl;
+}
+
+void Harl::info( void )
+{
+	std::cout << "I cannot believe adding extra bacon costs more money." <<
+		"You didn’t put enough bacon in my burger! If you did, " <<
+		"I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning( void )
+{
+	std::cout << "I think I deserve to have some extra bacon for free. " <<
+		"I’ve been coming for years whereas you started working " <<
+		"here since last month." << std::endl;
+}
+
+void Harl::error( void )
+{
+	std::cout << "This is unacceptable! " <<
+	"I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain( std::string level )
+{
+	std::string choice[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	
+}
